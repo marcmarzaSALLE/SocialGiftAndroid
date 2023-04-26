7ab81feefde588c8e63327efdc961fc62c947898
Marc Marzá Osa <marc.marza@students.salle.url.edu>
2023-04-26 19:08:56 +0200

    empezando el diseño de la aplicación

--- app/src/main/java/com/example/socialgift/MainActivity.java
+++ app/src/main/java/com/example/socialgift/MainActivity.java
@@ -13,10 +13,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
 
         //Per fer proves del Login i Register
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+       Intent intent = new Intent(this, LoginActivity.class);
+       startActivity(intent);
 
         Log.wtf("MainActivity", String.valueOf(getApplicationContext()));
         Log.d("MainActivity", "onCreate: ");
