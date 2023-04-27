20157b5318d1bf6dac6c6d7acc5f1252d5c0e499
Martí <marti.ferretc@gmail.com>
2023-04-27 23:51:42 +0200

    Login-Register + BottomNavBar provisional

--- app/src/main/java/com/example/socialgift/MainActivity.java
+++ app/src/main/java/com/example/socialgift/MainActivity.java
@@ -1,23 +1,54 @@
 package com.example.socialgift;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
-import com.example.socialgift.activities.LoginActivity;
 import com.example.socialgift.controller.SharedPreferencesController;
-import com.example.socialgift.dao.VolleyRequest;
+import com.example.socialgift.fragments.ChatFragment;
+import com.example.socialgift.fragments.ListFragment;
+import com.example.socialgift.fragments.UserFragment;
+
+import me.ibrahimsn.lib.OnItemSelectedListener;
+import me.ibrahimsn.lib.SmoothBottomBar;
 
 public class MainActivity extends AppCompatActivity {
 
     private SharedPreferencesController sharedPreferencesController;
+    private SmoothBottomBar bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        replace(new ListFragment());
+
+        bottomNavigationView.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public boolean onItemSelect(int i) {
+                switch (i){
+                    case 0:
+                        replace(new ListFragment());
+                        break;
+
+                    case 1:
+                        replace(new ChatFragment());
+                        break;
+
+                    case 2:
+                        replace(new UserFragment());
+                        break;
+                }
+                return true;
+            }
+        });
+
+        /*
         sharedPreferencesController = new SharedPreferencesController();
         if(sharedPreferencesController.loadDateSharedPreferences(getApplicationContext()) != null){
             Toast.makeText(getApplicationContext(), "AUTORIZADO", Toast.LENGTH_SHORT).show();
@@ -26,5 +57,12 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
             startActivity(intent);
         }
+
+         */
+    }
+    private void replace(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame,fragment);
+        transaction.commit();
     }
 }
