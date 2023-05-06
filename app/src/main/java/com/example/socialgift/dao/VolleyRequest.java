e668901c5cce2dd3242521ff4aae87787dd4afa6
Marc Marzá Osa <marc.marza@students.salle.url.edu>
2023-05-07 00:25:53 +0200

    Ya se pueden crear listas, falta poder añadir y crear regalos y mostrar las listas.

--- app/src/main/java/com/example/socialgift/dao/VolleyRequest.java
+++ app/src/main/java/com/example/socialgift/dao/VolleyRequest.java
@@ -24,6 +24,7 @@ public class VolleyRequest {
 
     private final String searchParameter = "/search?s=";
     private final String loginParameter = "/login";
+    private final String whishlistParameter = "/wishlists";
     private final RequestQueue queue;
     private final JSONObject jsonBody;
     private final Context context;
@@ -80,21 +81,21 @@ public class VolleyRequest {
         queue.add(jsonArrayRequest);
     }
 
-    public void getMyUser(int id,Response.Listener<JSONObject> findMyUser,Response.ErrorListener errorListener){
+    public void getMyUser(int id, Response.Listener<JSONObject> findMyUser, Response.ErrorListener errorListener) {
         String createUrl = this.url + this.userParameter + "/" + id;
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, createUrl,null,findMyUser,errorListener){
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, createUrl, null, findMyUser, errorListener) {
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String,String> headers = new HashMap<>();
+                Map<String, String> headers = new HashMap<>();
 
-                headers.put("Authorization","Bearer " + sharedPreferencesController.loadDateSharedPreferences(context));
+                headers.put("Authorization", "Bearer " + sharedPreferencesController.loadDateSharedPreferences(context));
                 return headers;
             }
         };
         queue.add(jsonObjectRequest);
     }
 
-    public void editMyUser(String name,String last_name,String email,String password,String image,Response.Listener<JSONObject> editMyUser,Response.ErrorListener errorListener){
+    public void editMyUser(String name, String last_name, String email, String password, String image, Response.Listener<JSONObject> editMyUser, Response.ErrorListener errorListener) {
         String createUrl = this.url + this.userParameter;
         try {
             jsonBody.put("name", name);
@@ -102,12 +103,32 @@ public class VolleyRequest {
             jsonBody.put("email", email);
             jsonBody.put("password", password);
             jsonBody.put("image", image);
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PUT, createUrl, jsonBody, editMyUser, errorListener){
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PUT, createUrl, jsonBody, editMyUser, errorListener) {
                 @Override
                 public Map<String, String> getHeaders() throws AuthFailureError {
-                    Map<String,String> headers = new HashMap<>();
+                    Map<String, String> headers = new HashMap<>();
 
-                    headers.put("Authorization","Bearer " + sharedPreferencesController.loadDateSharedPreferences(context));
+                    headers.put("Authorization", "Bearer " + sharedPreferencesController.loadDateSharedPreferences(context));
+                    return headers;
+                }
+            };
+            queue.add(jsonObjectRequest);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void addNewList(String name, String description, String date, Response.Listener<JSONObject> addNewList, Response.ErrorListener errorListener) {
+        String createUrl = this.url + this.whishlistParameter;
+        try {
+            jsonBody.put("name", name);
+            jsonBody.put("description", description);
+            jsonBody.put("end_date", date);
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, createUrl, jsonBody, addNewList, errorListener) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> headers = new HashMap<>();
+                    headers.put("Authorization", "Bearer " + sharedPreferencesController.loadDateSharedPreferences(context));
                     return headers;
                 }
             };
--- app/src/main/java/com/example/socialgift/fragments/AddListFragment.java
+++ app/src/main/java/com/example/socialgift/fragments/AddListFragment.java
@@ -1,5 +1,6 @@
 package com.example.socialgift.fragments;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,14 +11,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentManager;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.example.socialgift.R;
+import com.example.socialgift.dao.VolleyRequest;
+
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Objects;
 
 public class AddListFragment extends Fragment {
     private Toolbar toolbar;
@@ -25,12 +38,15 @@ public class AddListFragment extends Fragment {
     private TextView txtListName, txtDeleteList, txtAddGift;
     private EditText edtTxtListName, edtTxtDescription, edtTxtDate;
     private Button btnSaveList, btnAddGift;
+    private VolleyRequest volleyRequest;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add_list, container, false);
-        syncronizeView();
+        volleyRequest = new VolleyRequest(requireActivity().getApplicationContext());
+        syncronizeViewToolbar();
+        syncronizeViewWidgets(view);
         showInfoToolbar();
 
         imgBtnBack.setOnClickListener(new View.OnClickListener() {
@@ -41,10 +57,36 @@ public class AddListFragment extends Fragment {
                 fragmentManager.beginTransaction().replace(R.id.frame, new ListFragment()).commit();
             }
         });
+        edtTxtDate.setOnClickListener(v ->{
+                showDateDialog();
+        });
+        btnSaveList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.wtf("AddListFragment", "Save button clicked");
+                if(checkData()){
+                    if(dateIsCorrect()){
+                        volleyRequest.addNewList(edtTxtListName.getText().toString(), edtTxtDescription.getText().toString(), edtTxtDate.getText().toString(), new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Toast.makeText(requireActivity().getApplicationContext(), "Lista creada correctamente", Toast.LENGTH_SHORT).show();
+                                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                                fragmentManager.beginTransaction().replace(R.id.frame, new ListFragment()).commit();
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(requireActivity().getApplicationContext(), "Error al crear la lista", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }
+            }
+        });
         return view;
     }
 
-    private void syncronizeView() {
+    private void syncronizeViewToolbar() {
         toolbar = (Toolbar) requireActivity().findViewById(R.id.toolbar);
         ((AppCompatActivity) requireActivity()).setSupportActionBar(toolbar);
         txtListName = (TextView) toolbar.findViewById(R.id.toolbar_title);
@@ -53,9 +95,67 @@ public class AddListFragment extends Fragment {
         imgBtnAddList = (ImageButton) toolbar.findViewById(R.id.toolbar_main_button_logout);
     }
 
+    private void syncronizeViewWidgets(View view){
+        edtTxtListName = (EditText) view.findViewById(R.id.edtNameList);
+        edtTxtDescription = (EditText) view.findViewById(R.id.edtDescription);
+        edtTxtDate = (EditText) view.findViewById(R.id.edtEndDate);
+        btnSaveList = (Button) view.findViewById(R.id.btnSaveList);
+    }
+    private void showDateDialog() {
+        DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                final String selectedDate;
+                Calendar cal = Calendar.getInstance();
+                cal.set(year, month, day);
+                String format = "yyyy-MM-dd";
+                selectedDate = new SimpleDateFormat(format).format(cal.getTime());
+                edtTxtDate.setText(selectedDate);
+            }
+        });
+
+        newFragment.show(getParentFragmentManager(), "datePicker");
+    }
     private void showInfoToolbar() {
         imgBtnAddList.setVisibility(View.INVISIBLE);
         imgBtnBack.setVisibility(View.VISIBLE);
         txtDeleteList.setText(getResources().getText(R.string.delete_list));
+    }
+
+    private boolean checkData(){
+        if(edtTxtListName.getText().toString().isEmpty()){
+            edtTxtListName.setError(getResources().getText(R.string.name_list_required));
+            return false;
+        }else if(edtTxtDescription.getText().toString().isEmpty()){
+            edtTxtDescription.setError(getResources().getString(R.string.description_required));
+            return false;
+
+        } else if (edtTxtDate.getText().toString().isEmpty()) {
+            edtTxtDate.setError(getResources().getString(R.string.end_date_required));
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean dateIsCorrect(){
+        boolean correctDate = false;
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Calendar dateTimeCal = Calendar.getInstance();
+        String dateText = edtTxtDate.getText().toString();
+        String timeText = "11:59:59";
+        String finalDate = dateText + " " + timeText;
+        try {
+            dateTimeCal.setTime(Objects.requireNonNull(dateFormat.parse(finalDate)));
+            if (dateTimeCal.compareTo(calendar) > 0) {
+                correctDate = true;
+            } else {
+                edtTxtDate.setError(getResources().getString(R.string.errorDate));
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return correctDate;
     }
 }
